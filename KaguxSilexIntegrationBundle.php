58e0dcdb0f06f53d37cbee54ea3259b581127ddf
boris <boris@exoclick.com>
2012-11-22 11:30:21 +0100

    it is not possible to use services integrated using "create" mode as middleware functions. Added exception catcher with explanation for this problem

--- KaguxSilexIntegrationBundle.php
+++ KaguxSilexIntegrationBundle.php
@@ -3,6 +3,7 @@
 namespace Kagux\SilexIntegrationBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Kagux\SilexIntegrationBundle\Silex\Integration\SilexIntegrationException;
 use Kagux\SilexIntegrationBundle\DependencyInjection\Compiler\AddIntegratorsPass;
 use Kagux\SilexIntegrationBundle\DependencyInjection\Compiler\TwigFormIntegrationPass;
 use Kagux\SilexIntegrationBundle\DependencyInjection\Compiler\SilexAppServiceIntegrationPass;
@@ -23,7 +24,15 @@ class KaguxSilexIntegrationBundle extends Bundle
 
     public function boot()
     {
+        try{
         $pool = $this->container->get('silex.integration.service.pool');
+        }
+        catch( \InvalidArgumentException $e){
+            throw new SilexIntegrationException($e->getMessage(). ' '.
+             'Please, ensure that you don\'t use Symfony2 services integrated in "create" '.
+             'mode as middleware functions. Because Silex tries to load them before we had '.
+             'a chance to integrate requested service. You can wrap it in a closure though!');
+        }
         $pool->integrate();
     }
 
